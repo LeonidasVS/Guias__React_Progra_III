de9c2964dbe302dd64a0ceb3480153787580eddb
LeonidasVS <villafrancosibrian@gmail.com>
2025-02-01 19:39:37 -0600

    Guia 6 | Introduccion a ASP.NET

--- ReactBackend/obj/Debug/net8.0/ReactBackend.AssemblyInfo.cs
+++ ReactBackend/obj/Debug/net8.0/ReactBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReactBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dd5e289824dd2fa10126324f2f7805fec71a9ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db8454a8062afa1bc5560736110406a4e47994dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReactBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReactBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ WebApi/Controllers/PruebaControllers.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PruebaControllers : ControllerBase
+    {
+        [HttpGet("Prueba")]
+
+        public string Get() {
+            return "Hola mundo";        
+        }
+    }
+}
